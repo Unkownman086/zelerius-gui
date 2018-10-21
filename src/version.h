50b2c0f0b3b8054e5b9466711462ee75654110eb
Vic <nilhcraiv@gmail.com>
2018-10-21 16:54:56 +0200

    v1.2.1

---
+++ src/version.h
@@ -0,0 +1,12 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+namespace WalletGUI {
+
+constexpr char VERSION[] = "1.2.1";
+constexpr char VERSION_SUFFIX[] = "stable";
+constexpr char REVISION[] = "20181021";
+
+}
+
+#endif // VERSION_H
